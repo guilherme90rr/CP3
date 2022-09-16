feaf47279873efd930c78fdd1267f040bf13f112
Guilherme Ferreira <101301922+guilherme90rr@users.noreply.github.com>
2022-09-15 21:07:49 -0300

    Add files via upload

---
+++ CP_3/EX02/PessoaFisica.java
@@ -0,0 +1,16 @@
+package EX02;
+
+public class PessoaFisica extends Pessoa {
+
+	private int cpf;
+
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public PessoaFisica() {
+		
+	}
+}
---
+++ CP_3/EX07/Conta.java
@@ -0,0 +1,64 @@
+package EX07;
+
+import java.util.Scanner;
+
+public abstract class Conta implements IConta {
+	
+	Scanner ler = new Scanner(System.in);
+
+    private String agencia;
+    private String numero;
+    private double saldo;
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    // metodos interface
+    
+    public String mostrarSaldo() {
+    	return "Seu saldo � de: " + saldo;
+    }
+    
+    public String sacar(double valor) {
+    	if (saldo != 0) {
+    		System.out.printf("Digite o quanto gostaria de sacar neste momento => \n");
+    		valor = ler.nextDouble();
+    		
+    		String resposta;
+    		
+    		if (valor < saldo) {
+    			
+    			this.saldo = saldo - valor;
+    			return "Transacao aprovada, seu saldo atual  de: " + saldo;
+    		
+    			
+    		} else {
+    			return "Voce nao possui saldo suficiente.";
+    		}
+    	}
+    	
+		return "Transacao finalizada";
+    }
+
+}
